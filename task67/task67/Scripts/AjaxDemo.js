fbe18a6275b6f66a1cc7743cde5844d9f95e6b24
Budzyn <black_witcher@ukr.net>
2015-12-02 17:13:20 +0200

    1111: Ajax and jQuery

---
+++ task67/task67/Scripts/AjaxDemo.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#privacyLink').click(function (event) {
+        event.preventDefault();
+        var url = $(this).attr('href');
+        $('#privacy').load(url);
+    });
+});
